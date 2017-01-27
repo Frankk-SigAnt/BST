b45011921d103e7fe113a3292f2919e12fdec62e
Peng Guanwen <pgw1999@outlook.com>
2017-01-27 20:09:40 +0800

    Formatted by Visual Studio

--- bstree.h
+++ bstree.h
@@ -10,148 +10,148 @@ namespace fsa
     template<class T>
     class bstree
     {
-        protected:
-            struct bst_node
+    protected:
+        struct bst_node
+        {
+            T data;
+            bst_node * father;
+            //bst_node * left, right;
+            std::unique_ptr<bst_node> left, right;
+
+            bst_node() {}
+            bst_node(T & _val, bst_node * _pf = nullptr) : data(_val), father(_pf) {}
+        };
+
+        struct bst_iterator
+        {
+            bst_node * _ptr;
+
+            bst_iterator(bst_node * _Ptr = 0) : _ptr(_Ptr) {}
+            bst_iterator(bst_iterator & _it) : _ptr(_it.ptr) {}
+
+            T & operator*()
             {
-                T data;
-                bst_node * father;
-                //bst_node * left, right;
-                std::unique_ptr<bst_node> left, right;
+                return *_ptr->data;
+            }
 
-                bst_node() {}
-                bst_node(T & _val, bst_node * _pf = nullptr): data(_val), father(_pf) {}
-            };
-
-            struct bst_iterator
+            bst_node * operator->()
             {
-                bst_node * _ptr;
+                return _ptr;
+            }
 
-                bst_iterator(bst_node * _Ptr = 0): _ptr(_Ptr) {}
-                bst_iterator(bst_iterator & _it): _ptr(_it.ptr) {}
-
-                T & operator*()
+            bst_iterator & operator++()
+            {
+                if ((*this)->right != 0)
                 {
-                    return *_ptr->data;
-                }
-
-                bst_node * operator->()
-                {
-                    return _ptr;
-                }
-
-                bst_iterator & operator++()
-                {
-                    if((*this)->right != 0)
-                    {
-                        this->ptr = (*this)->right;
-                        while((*this)->left != 0)
-                        {
-                            this->ptr = (*this)->left;
-                        }
-                    }
-                    else
-                    {
-                        bst_node * prev = this->ptr;
-                        this->ptr = (*this)->father;
-                        while(prev == (*this)->right)
-                        {
-                            prev = this->ptr;
-                            this->ptr = (*this)->father;
-                        }
-                    }
-                    return *this;
-                }
-
-                bst_iterator operator++(int)
-                {
-                    bst_iterator tmp_iter = *this;
-                    *(this)++;
-                    return tmp_iter;
-                }
-
-                bst_iterator & operator--()
-                {
-                    if((*this)->left != 0)
+                    this->ptr = (*this)->right;
+                    while ((*this)->left != 0)
                     {
                         this->ptr = (*this)->left;
-                        while((*this)->right != 0)
-                        {
-                            this->ptr = (*this)->right;
-                        }
                     }
-                    else
+                }
+                else
+                {
+                    bst_node * prev = this->ptr;
+                    this->ptr = (*this)->father;
+                    while (prev == (*this)->right)
                     {
-                        bst_node * prev = this->ptr;
+                        prev = this->ptr;
                         this->ptr = (*this)->father;
-                        while(prev == (*this)->left)
-                        {
-                            prev = this->ptr;
-                            this->ptr = (*this)->father;
-                        }
                     }
-                    return *this;
                 }
-
-                bst_iterator operator--(int)
-                {
-                    bst_iterator tmp_iter = *this;
-                    *(this)--;
-                    return tmp_iter;
-                }
-
-                bool operator==(bst_iterator & _it)
-                {
-                    return _ptr == _it._ptr;
-                }
-
-                bool operator!=(bst_iterator & _it)
-                {
-                    return _ptr != _it._ptr;
-                }
-            };
-
-        public:
-            typedef T             value_type;
-            typedef T &           reference;
-            typedef bst_node      node_type;
-            typedef bst_node *    pointer;
-            typedef bst_iterator  iterator;
-            typedef unsigned int  size_type;
-
-        private:
-            std::unique_ptr<node_type> _header;
-            size_type _size;
-
-        protected:
-            iterator left_rotate(iterator _pos);
-            iterator right_rotate(iterator _pos);
-
-        public:
-            //Con-/De-structors and operator=
-            bstree(): _header(0), _size(0) {}
-            explicit bstree(value_type & _val): _header(0), _size(1)
-            {
-                _header->father = new node_type(_val, _header->_ptr);
-                _header->left = _header->right = _header->father;
+                return *this;
             }
-            virtual ~bstree();
-            bstree & operator=(bstree & _opr);
 
-            //Capasity and element access
-            size_type size() const {return _size;}
-            bool empty() const {return (_header->father == 0);}
-            iterator begin() {return iterator(_header->right);}
-            iterator end() {return iterator(_header);} 
-            reference front() const {return _header->right->data;}
-            reference back() const {return _header->left->data;}
-            iterator find(reference _val);
+            bst_iterator operator++(int)
+            {
+                bst_iterator tmp_iter = *this;
+                *(this)++;
+                return tmp_iter;
+            }
 
-            //Modifiers
-            void insert(reference _val);
-            void erase(iterator _pos);
-            void remove(reference _val);
-            void swap(bstree & _opr);
-            void splice(bstree & _opr);
+            bst_iterator & operator--()
+            {
+                if ((*this)->left != 0)
+                {
+                    this->ptr = (*this)->left;
+                    while ((*this)->right != 0)
+                    {
+                        this->ptr = (*this)->right;
+                    }
+                }
+                else
+                {
+                    bst_node * prev = this->ptr;
+                    this->ptr = (*this)->father;
+                    while (prev == (*this)->left)
+                    {
+                        prev = this->ptr;
+                        this->ptr = (*this)->father;
+                    }
+                }
+                return *this;
+            }
+
+            bst_iterator operator--(int)
+            {
+                bst_iterator tmp_iter = *this;
+                *(this)--;
+                return tmp_iter;
+            }
+
+            bool operator==(bst_iterator & _it)
+            {
+                return _ptr == _it._ptr;
+            }
+
+            bool operator!=(bst_iterator & _it)
+            {
+                return _ptr != _it._ptr;
+            }
+        };
+
+    public:
+        typedef T             value_type;
+        typedef T &           reference;
+        typedef bst_node      node_type;
+        typedef bst_node *    pointer;
+        typedef bst_iterator  iterator;
+        typedef unsigned int  size_type;
+
+    private:
+        std::unique_ptr<node_type> _header;
+        size_type _size;
+
+    protected:
+        iterator left_rotate(iterator _pos);
+        iterator right_rotate(iterator _pos);
+
+    public:
+        //Con-/De-structors and operator=
+        bstree() : _header(0), _size(0) {}
+        explicit bstree(value_type & _val) : _header(0), _size(1)
+        {
+            _header->father = new node_type(_val, _header->_ptr);
+            _header->left = _header->right = _header->father;
+        }
+        virtual ~bstree();
+        bstree & operator=(bstree & _opr);
+
+        //Capasity and element access
+        size_type size() const { return _size; }
+        bool empty() const { return (_header->father == 0); }
+        iterator begin() { return iterator(_header->right); }
+        iterator end() { return iterator(_header); }
+        reference front() const { return _header->right->data; }
+        reference back() const { return _header->left->data; }
+        iterator find(reference _val);
+
+        //Modifiers
+        void insert(reference _val);
+        void erase(iterator _pos);
+        void remove(reference _val);
+        void swap(bstree & _opr);
+        void splice(bstree & _opr);
     };
 
     template<class T>
@@ -165,21 +165,21 @@ namespace fsa
     {
         pointer _ptr = _header->father;
 
-        while(_ptr != 0)
+        while (_ptr != 0)
         {
-            if(_val == _ptr->data)
+            if (_val == _ptr->data)
             {
                 break;
             }
             else
             {
-                _ptr = (_val < _ptr->data) 
+                _ptr = (_val < _ptr->data)
                     ? _ptr->left
                     : _ptr->right;
             }
         }
 
-        if(_ptr == 0)
+        if (_ptr == 0)
         {
             return end();
         }
